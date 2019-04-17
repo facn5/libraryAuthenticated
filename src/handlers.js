2cc5673fb28d1486fa35196ae8b966565078d58b
karam1ashqar <karam.ashqar01@gmail.com>
2019-04-17 12:33:57 +0300

    Added a handler for check my cookei route

--- src/handlers.js
+++ src/handlers.js
@@ -4,7 +4,7 @@ const querystring = require("querystring");
 const getBooks = require("./queries/getBooks");
 const createUser = require("./queries/createUser");
 const loginUser = require("./queries/loginUser");
-const utils = require('./utils');
+const utils = require("./utils");
 const serverError = "500 server error";
 
 const exType = {
@@ -82,7 +82,6 @@ const handleCreateUser = (req, res) => {
       const parse = querystring.parse(body);
       utils.hash(parse.password, (err, hash) => {
         createUser(parse.name, parse.username, hash, (err, result) => {
-
           if (err) {
             if (err.message === "username_exist") {
               // console.log(res);
@@ -92,14 +91,14 @@ const handleCreateUser = (req, res) => {
             }
             return err;
           } else {
-            res.body
+            res.body;
             res.writeHead(302, {
               location: "/"
             });
             res.end();
           }
         });
-      })
+      });
     }
   });
 };
@@ -112,15 +111,23 @@ const handleUserLogin = (req, res) => {
   req.on("end", () => {
     if (body != null) {
       const parse = querystring.parse(body);
+
       loginUser(parse.username, parse.password, (err, result) => {
         if (err) console.log(err);
         else {
           if (result) {
-            res.writeHead(302, {
-              location: "/home"
-            });
+
+                const cryptoPass = utils.functions.sign(parse.password);
+
+            res.writeHead(302, [
+              ["location", "/home"],
+              ["Set-Cookie", "logged_in=true"],
+              [`Set-Cookie`, `username=${parse.username}`],
+              ['Set-Cookie', `password=${cryptoPass}`]
+            ]);
+
             res.end();
-          }else{
+          } else {
             res.writeHead(409);
             res.end("Invalid username or password");
           }
@@ -129,7 +136,6 @@ const handleUserLogin = (req, res) => {
     }
   });
 };
-
 
 const handleHome = (res, url) => {
   const filePath = path.join(__dirname, "..", url);
@@ -147,11 +153,29 @@ const handleHome = (res, url) => {
   });
 };
 
+const handleCheckTheCookie = ( req, res ) => {
+
+  let body = "";
+  req.on("data", chunk => {
+    body += chunk.toString();
+  });
+  req.on("end", () => {
+    if (body != null) {
+
+      console.log(body);
+      res.writeHead(200, exType.html)
+      res.end(body);
+
+    }
+  })
+}
+
 module.exports = {
   page: handlePage,
   file: handlePublic,
   getbooks: handleGetBooks,
   createUser: handleCreateUser,
   login: handleUserLogin,
+  checkcookie: handleCheckTheCookie,
   home: handleHome
 };
