af7c0a8a15863a816397c014cc990ac354b1c046
karam1ashqar <karam.ashqar01@gmail.com>
2019-04-17 12:32:23 +0300

    Added cookie post route fetch

--- public/scripts/fetch.js
+++ public/scripts/fetch.js
@@ -1,10 +1,32 @@
-function getBooks() {
+function getBooks(cb) {
   fetch('/getBooks')
     .then(function(response) {
       return response.json();
     })
     .then(function(myJson) {
-      updatedom(myJson);
+      cb(myJson);
+    })
+  .catch(function(error) {
+    return error;
+  });
+}
+
+function checkCookies(cb, pass) {
+  fetch('/checkmycookie', {
+        method: "POST",
+        mode: "cors",
+ // *GET, POST, PUT, DELETE, etc.
+        headers: {
+            "Content-Type": "text/plain",
+            // "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: JSON.stringify(pass) // body data type must match "Content-Type" header
+    })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      cb(data);
     })
   .catch(function(error) {
     return error;
